9d9444e0855bdd625df0b5727add15a32e4e13b6
Sebastián Cardozo <sebastian.cardozo97@gmail.com>
2021-03-14 23:57:12 -0500

    Rollback

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
 	http.ListenAndServe(os.Getenv("DB_PORT"), ChiRouter().InitRouter())
 }
