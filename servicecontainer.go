b63aee0a874f8fc1160deb4cfab41f49f0e33701
Sebastián Cardozo <sebastian.cardozo97@gmail.com>
2021-03-15 00:14:30 -0500

    SSL mode

--- servicecontainer.go
+++ servicecontainer.go
@@ -20,8 +20,8 @@ type IServiceContainer interface {
 type kernel struct{}
 
 func (k *kernel) InjectTopSecretController() controllers.TopSecretController {
-	strConn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	strConn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_SSL_MODE"))
 
 	pgConn, err := sql.Open("postgres", strConn)
 
